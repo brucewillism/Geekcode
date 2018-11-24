4f3bcbcdd001d92691545629adaf11b8af813d60
Bruce Willis <bruce.willis.br07@gmail.com>
2018-11-24 18:03:47 -0300

    criação do projeto

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>GEEKCODE</title>
+  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+  <link href='https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,400italic,300italic,300|Raleway:300,400,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/animate.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div class="content">
+    <div class="container wow fadeInUp delay-03s">
+      <div class="row">
+        <div class="logo text-center">
+          <img src="img/logo.png" alt="logo" width="150">
+          <h2>Formando o Cidadão Do Seculo 21</h2>
+          <p>Você Não Pode Ficar De Fora</p>
+        </div>
+      </div>
+    </div>
+    <section id="about" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 text-center">
+            <div class="about-title">
+              <h2>O que é Robotica Educacional?</h2>
+              <p>Na Geekcode o ensino da robotica vai muito alem da programação e projetos com robôs com uma dinamica pedagógica<br> que propociona um aprendizado pratico que desenvolve a cogniçao e a capacidade do aluno de pensar<br></p>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="about-title">
+              <h2>Planos</h2>
+              <p>Aqui na Geekcode podemos adptar a realidade da escola com isso a parceiria firmada tará para a instituiçao de ensino e os<br> alunos o aliado perfeito no desenvolvimento das competencias desejadas</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-02s">
+              <div class="img">
+                <i class="fa fa-refresh"></i>
+              </div>
+              <h3 class="abt-hd">1 ano fundamental ao 4 ano fundamental</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-04s">
+              <div class="img">
+                <i class="fa fa-eye"></i>
+              </div>
+              <h3 class="abt-hd">5 ano ao 9 ano</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-06s">
+              <div class="img">
+                <i class="fa fa-cogs"></i>
+              </div>
+              <h3 class="abt-hd">1 ano medio ao 3 ano medio</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-08s">
+              <div class="img">
+                <i class="fa fa-dot-circle-o"></i>
+              </div>
+              <h3 class="abt-hd">Nivel Tecnico</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <?php include 'rodape.php'; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+
+  </html>
---
+++ rodape.php
@@ -0,0 +1,38 @@
+    <footer class="footer">
+      <div class="container">
+        <div class="row bort">
+
+          <section>
+            <div class="container">
+              <div class="row bort text-center">
+                <div class="social">
+                  <ul>
+                    <li>
+                      <a href=""><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href=""><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href=""><i class="fa fa-linkedin"></i></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div class="copyright">
+            © Copyright geekcode todos os direitos reservados.
+            <div class="credits">
+              Visiti-nos <a href="https:geekcode.net.com/">GeenkCode</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/custom.js"></script>
